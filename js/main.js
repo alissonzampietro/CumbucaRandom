0b7149c9f475ae6a3376dc11fad7c9c406f2b2b8
Alisson Zampietro <alisson.zampietro@gmail.com>
2017-05-23 19:29:27 -0300

    Refatorando projeto

--- js/main.js
+++ js/main.js
@@ -1,23 +1,31 @@
-window.onload = function()
-{
-	var nomes = ['Bem vindo','Welcome','Bienvenido'];
+let nomes = ['Bem vindo','Welcome','Bienvenido'];
+
+let generateRandomName = () => {
+	let campoNome = document.getElementById('nomes').value;
+	let flagNome = campoNome.split('-');
+
+	if(flagNome.length > 1)
+		nomes = flagNome;
+
+	let intervalo = setInterval(() => {
+		let rand = nomes[Math.floor(Math.random() * nomes.length)];
+		document.getElementById('nomes').value = rand;
+	},20);
+
+	setTimeout(() => {
+		clearInterval(intervalo);
+	},2000);
+}
+
+window.onload = () => {
 	let iniciar = document.getElementById('iniciar');
 
 	iniciar.addEventListener('click', () => {
-
-		let campoNome = document.getElementById('nomes').value;
-		let flagNome = campoNome.split('-');
-
-		if(flagNome.length > 1)
-			nomes = flagNome;
-
-		let intervalo = setInterval(function() {
-			let rand = nomes[Math.floor(Math.random() * nomes.length)];
-			document.getElementById('nomes').value = rand;
-		},20);
-
-		setTimeout(function() {
-			clearInterval(intervalo);
-		},2000);
+		generateRandomName();
 	});
+
+	window.onkeypress = (e) => {
+		if(e.charCode === 13)
+			generateRandomName();
+	}
 }
