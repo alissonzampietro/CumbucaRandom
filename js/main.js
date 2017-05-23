a64120cd5ff30efa3ab59b9da860b0a924d680c2
Alisson Zampietro <alisson.zampietro@gmail.com>
2017-05-23 09:44:52 -0300

    Removendo os espaços di split

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ window.onload = function()
 			return false;
 
 		if(nomes.length === 0) 
-			nomes = campo.value.split(' - ');
+			nomes = campo.value.split('-');
 
 		let intervalo = setInterval(function() {
 			let rand = nomes[Math.floor(Math.random() * nomes.length)];
