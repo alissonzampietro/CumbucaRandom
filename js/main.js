d1d30074ba39f4940e12b465de7878f0cbba0388
Alisson Zampietro <alisson.zampietro@gmail.com>
2017-05-25 15:38:28 -0300

    Inserindo maior randomicidade ao sorteio

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 let nomes = ['Bem vindo','Welcome','Bienvenido'];
 
+let shuffle =  (o) => {
+    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
+
+
 let generateRandomName = () => {
 	let campoNome = document.getElementById('nomes').value;
 	let flagNome = campoNome.split('-');
@@ -8,7 +14,9 @@ let generateRandomName = () => {
 		nomes = flagNome;
 
 	let intervalo = setInterval(() => {
-		let rand = nomes[Math.floor(Math.random() * nomes.length)];
+		let novoNome = shuffle(nomes);
+		let rand = novoNome[Math.floor(Math.random() * novoNome.length)];
+		console.log(shuffle(nomes));
 		document.getElementById('nomes').value = rand;
 	},20);
 
